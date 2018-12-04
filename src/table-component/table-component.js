0f6065e87721958058ed9f6619639ce361fba215
tserekhivan <tserekh.ivan@gmail.com>
2018-12-05 00:59:31 +0300

    feature: add table sorting

--- src/table-component/table-component.js
+++ src/table-component/table-component.js
@@ -1,4 +1,5 @@
 import template from './table-component.html';
+import './table-component.css';
 
 export default class TableComponent {
   constructor(data) {
@@ -30,6 +31,69 @@ export default class TableComponent {
     tableRows.forEach((row) => {
       tbody.appendChild(row);
     });
+    this.addEvents();
     return this.table;
+  }
+
+  addEvents() {
+    this.table.getElementsByTagName('thead')[0].onclick = (event) => {
+      if (event.target.classList.contains('fas')) {
+        if (!event.target.classList.contains('current-sorting')) {
+          this.table.querySelector('.current-sorting').classList.remove('current-sorting');
+          // eslint-disable-next-line prefer-destructuring
+          const cellIndex = event.target.offsetParent.cellIndex;
+          if (event.target.classList.contains('fa-sort-up')) {
+            this.sortTable(cellIndex, '>');
+          } else if (event.target.classList.contains('fa-sort-down')) {
+            this.sortTable(cellIndex, '<');
+          }
+          event.target.classList.add('current-sorting');
+        }
+      }
+    };
+  }
+
+  sortTable(numberColumn, comparator) {
+    window.console.log(numberColumn);
+    const len = this.table.getElementsByTagName('tbody')[0].children.length;
+    const array = Array.from(this.table.getElementsByTagName('tbody')[0].rows);
+    if (comparator === '>') {
+      if (numberColumn === 0) {
+        // eslint-disable-next-line max-len
+        array.sort((a, b) => +a.children[numberColumn].innerHTML - +b.children[numberColumn].innerHTML);
+      } else {
+        array.sort((a, b) => {
+          if (a.children[numberColumn].innerHTML > b.children[numberColumn].innerHTML) {
+            return 1;
+          }
+          if (a.children[numberColumn].innerHTML < b.children[numberColumn].innerHTML) {
+            return -1;
+          }
+          return 0;
+        });
+      }
+    } else if (comparator === '<') {
+      if (numberColumn === 0) {
+        // eslint-disable-next-line max-len
+        array.sort((a, b) => +a.children[numberColumn].innerHTML + +b.children[numberColumn].innerHTML);
+      } else {
+        array.sort((a, b) => {
+          if (a.children[numberColumn].innerHTML < b.children[numberColumn].innerHTML) {
+            return 1;
+          }
+          if (a.children[numberColumn].innerHTML > b.children[numberColumn].innerHTML) {
+            return -1;
+          }
+          return 0;
+        });
+      }
+    }
+    const tmp = [];
+    array.forEach((item, index) => {
+      tmp[index] = item.innerHTML;
+    });
+    for (let i = 0; i < len; i += 1) {
+      this.table.getElementsByTagName('tbody')[0].querySelector(`tr:nth-child(${i + 1})`).innerHTML = tmp[i];
+    }
   }
 }
